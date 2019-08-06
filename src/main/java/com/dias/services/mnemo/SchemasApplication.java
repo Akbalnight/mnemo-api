4b5caa5b7094be532b237b226f5cf616f9618d5e
Anton Eliseev <av.eliseev@i-teco.ru>
2019-08-06 16:31:15 +0300

    SchemasApplication.java edited online with Bitbucket

--- src/main/java/com/dias/services/mnemo/SchemasApplication.java
+++ src/main/java/com/dias/services/mnemo/SchemasApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication(scanBasePackages = {"com.dias.services.mnemo"})
+@SpringBootApplication(scanBasePackages = {"com.dias.services.mnemo", "com.common"})
 public class SchemasApplication {
 
     public static void main(String[] args) {
